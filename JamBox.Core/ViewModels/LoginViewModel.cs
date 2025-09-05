d185aa9fc473336517809eadfe6841b536936a6e
Jorge Ramirez <ramirez.jorgea@outlook.com>
2025-09-05 10:20:53 -0700

    Add assets

--- JamBox.Core/ViewModels/LoginViewModel.cs
+++ JamBox.Core/ViewModels/LoginViewModel.cs
@@ -17,14 +17,14 @@ public class LoginViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _serverUrl, value);
     }
 
-    private string _username = string.Empty;
+    private string _username = "jorgedevs";
     public string Username
     {
         get => _username;
         set => this.RaiseAndSetIfChanged(ref _username, value);
     }
 
-    private string _password = string.Empty;
+    private string _password = "pass";
     public string Password
     {
         get => _password;
