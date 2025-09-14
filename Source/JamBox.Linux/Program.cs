78edd8690900e0b9634a4e0de94876e9f78fb950
Adrian Stevens <adrian.stevens@gmail.com>
2025-09-14 13:29:54 -0700

    Linux head project cleanup

--- Source/JamBox.Linux/Program.cs
+++ Source/JamBox.Linux/Program.cs
@@ -1,20 +1,14 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using JamBox.Core;
-using System;
 
 namespace JamBox;
 
 internal class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
-    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
