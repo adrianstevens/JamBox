5bd1db9619ba8db1c6ae5b4d1e6c8585b5c66db5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2026-01-16 23:21:05 -0800

    Update miniplayer

--- Source/JamBox.Core/Views/UserControls/MiniPlayerView.axaml.cs
+++ Source/JamBox.Core/Views/UserControls/MiniPlayerView.axaml.cs
@@ -1,5 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Input;
 
 namespace JamBox.Core.Views.UserControls;
 
@@ -8,22 +7,5 @@ public partial class MiniPlayerView : UserControl
     public MiniPlayerView()
     {
         InitializeComponent();
-    }
-
-    private void Seek_OnPointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        if (DataContext is ViewModels.LibraryViewModel vm)
-        {
-            vm.Playback.IsUserSeeking = true;
-        }
-    }
-
-    private void Seek_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
-    {
-        if (DataContext is ViewModels.LibraryViewModel vm)
-        {
-            vm.Playback.IsUserSeeking = false;
-            vm.Playback.SeekPosition = SeekBar.Value;
-        }
     }
 }
